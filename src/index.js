a1ca91f8fb3beff75e19a943dd379b384e6eba4d
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-06-22 22:53:07 +0300

    🎨 Format with airbnb rules

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // Chakra-UI
 import { ChakraProvider } from '@chakra-ui/react'
-// import { Audio } from 'chakra-player'
+
 // Components
 import Audio from './lib/screens/Root'
+
 const container = document.getElementById('root')
 const root = createRoot(container)
 
@@ -16,4 +17,4 @@ root.render(
   </StrictMode>
 )
 
-export {Audio}
+export default Audio
--- src/lib/components/slider/SliderV.js
+++ src/lib/components/slider/SliderV.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
-import { Img, IconButton, Center } from '@chakra-ui/react';
-import vol1 from './vol1.svg';
-import vol2 from './vol2.svg';
-import vol3 from './vol3.svg';
-import vol4 from './vol4.svg';
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Img, IconButton, Center } from '@chakra-ui/react'
+import vol1 from './vol1.svg'
+import vol2 from './vol2.svg'
+import vol3 from './vol3.svg'
+import vol4 from './vol4.svg'
 
 function SliderV({ updateGlobalNum }) {
-  const [music, setMusic] = useState(vol3);
-  const [count, setCount] = useState(2);
+  const [music, setMusic] = useState(vol3)
+  const [count, setCount] = useState(2)
 
-  const handleLoudChange = e => {
+  const handleLoudChange = () => {
     if (count === 0) {
-      setMusic(vol2);
-      setCount(count + 1);
-      updateGlobalNum(33);
+      setMusic(vol2)
+      setCount(count + 1)
+      updateGlobalNum(33)
     } else if (count === 1) {
-      setMusic(vol3);
-      setCount(count + 1);
-      updateGlobalNum(66);
+      setMusic(vol3)
+      setCount(count + 1)
+      updateGlobalNum(66)
     } else if (count === 2) {
-      setMusic(vol4);
-      setCount(count + 1);
-      updateGlobalNum(100);
+      setMusic(vol4)
+      setCount(count + 1)
+      updateGlobalNum(100)
     } else if (count === 3) {
-      setMusic(vol1);
-      setCount(0);
-      updateGlobalNum(0);
+      setMusic(vol1)
+      setCount(0)
+      updateGlobalNum(0)
     }
-  };
+  }
   return (
     <IconButton
       boxSize="2em"
@@ -39,7 +40,11 @@ function SliderV({ updateGlobalNum }) {
         <Img src={music} alt="muz" />
       </Center>
     </IconButton>
-  );
+  )
 }
 
-export default SliderV;
+SliderV.propTypes = {
+  updateGlobalNum: PropTypes.func.isRequired,
+}
+
+export default SliderV
